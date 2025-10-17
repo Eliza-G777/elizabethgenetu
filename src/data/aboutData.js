b857d8bf23c8b847ff0841f5ee4d462505d0281c
Elizabeth.G <info.elsabetgenetu@gmail.com>
2025-10-17 00:31:47 -0500

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
-    title: "Who I Am",
-    description1: "My name is Elizabeth G. Taye. I’m a Grade 10 student at Lemlem Academy and an AddisCoder 2023 alumna, currently also a Yale Young African Scholar (YYAS) 2025.",
+    title: "Who I Am",5 
+    description1: "My name is Elizabeth G. Taye. I’m a Grade 11 Highschool Junior student at Lemlem Academy. I'm an AddisCoder 2023, Yale Young African Scholar (YYAS) 2025 alumunus and Yale Youth Global Scholar (YYGS) 2026 Aspirant",
     description2: "I’m a passionate STEM enthusiast—quick to self-learn new technologies, strong in mathematics and problem-solving, and driven by leadership and scientific communication.",
     image: 1
   }
